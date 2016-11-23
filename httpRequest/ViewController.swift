ae881117b77e85cad95ab63dd3072866ed0cab60
Vincent Clerissj <vincent.clerissj@gmail.com>
2016-11-23 15:43:48 +0100

    Initial Commit

---
+++ httpRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  httpRequest
+//
+//  Created by Vincent Clerissj on 23/11/2016.
+//  Copyright © 2016 Vincent Clerissj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
